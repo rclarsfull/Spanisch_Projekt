13668ee89dd227672d1c16e71f90af195222c122
rclarsfull <l.p.1999@live.de>
2020-09-12 20:03:23 +0200

    Speichern und laden eingebunden

---
+++ src/SpeichernUndLadenTXT.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SpeichernUndLadenTXT {
+
+
+    public static boolean speichern(String daten,String location,boolean notOverwrite) throws IOException {
+       boolean ausgabe=true;
+        try {
+            FileWriter schreiber = new FileWriter(location, notOverwrite);
+            schreiber.write(daten);
+            schreiber.close();
+        }catch (Exception e){
+            System.out.println("fehler bei speichen");
+            ausgabe=false;
+        }
+        return ausgabe;
+    }
+
+    public static String read(String location){
+        String zeile,ausgabe="";
+        try {
+            FileReader leser=new FileReader(location);
+            BufferedReader bufferedReader=new BufferedReader(leser);
+            while ((zeile=bufferedReader.readLine())!= null){
+                ausgabe+=zeile;
+            }
+        }catch(Exception e){
+            int l=ausgabe.length();
+            ausgabe="ERROR bei laenge: "+l;
+        }
+        return ausgabe;
+    }
+
+
+}
--- src/main.java
+++ src/main.java
@@ -1,12 +1,15 @@
 import javax.imageio.stream.FileImageInputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 
 public class main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+
+        String ort="test.txt";
 
         Wort test1=new Wort("bailar","tanzen",'v');
         Wort test2=new Wort("comer","essen",'v');
@@ -16,13 +19,14 @@ public class main {
         liste.add(test2);
         liste.add(test3);
 
-
+String text="";
         for (Wort a:
              liste) {
             System.out.println(a.toString());
+            text+=a.toString();
         }
 
-
+SpeichernUndLadenTXT.speichern(text,ort,true);
 
 
     }
